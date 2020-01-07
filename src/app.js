e4dcc4109474b0372232271b1b90a73146c2985e
SolodkayaT <tatasladkaya@gmail.com>
2020-01-07 21:02:30 +0200

    add scroll

--- src/app.js
+++ src/app.js
@@ -7,29 +7,37 @@ const refs = {
   loadMoreBtn: document.querySelector('button[data-action="load-more"]'),
 };
 
-refs.searchForm.addEventListener('submit', searchFormSubmitHandler);
-refs.loadMoreBtn.addEventListener('click', handleLoadMoreButton);
-
 function buildListImageMarkup(items) {
   const markup = items.map((item) => imageTamplate(item)).join('');
   refs.galeryList.insertAdjacentHTML('afterbegin', markup);
 }
 
-function searchFormSubmitHandler(event) {
-  event.preventDefault();
-  const inputValue = event.currentTarget.elements.query.value;
-  clearListImages();
-  apiService.resetPage();
-  apiService.searchQuery = inputValue;
-  apiService.fetchImages().then((data) => {
-    buildListImageMarkup(data);
-  });
-}
-function handleLoadMoreButton() {
-  apiService.fetchImages().then((data) => {
-    buildListImageMarkup(data);
-  });
-}
 function clearListImages() {
   refs.galeryList.innerHTML = '';
 }
+
+function scrollImage() {
+  setTimeout(() => {
+    const currentScroll = document.documentElement.scrollHeight + innerHeight;
+    window.scrollTo(0, currentScroll);
+  }, 1500);
+}
+
+function searchFormSubmitHandler(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const input = form.elements.query;
+  const inputValue = input.value;
+  clearListImages();
+  apiService.resetPage();
+  apiService.searchQuery = inputValue;
+  apiService.fetchImages().then((data) => buildListImageMarkup(data));
+  input.value = '';
+}
+function handleLoadMoreButton() {
+  apiService.fetchImages().then((data) => buildListImageMarkup(data));
+  scrollImage();
+}
+
+refs.searchForm.addEventListener('submit', searchFormSubmitHandler);
+refs.loadMoreBtn.addEventListener('click', handleLoadMoreButton);
