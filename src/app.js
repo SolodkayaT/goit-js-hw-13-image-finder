7d0d3fff717698761336f9b1d494aaa05e8c2628
SolodkayaT <tatasladkaya@gmail.com>
2020-01-07 19:17:21 +0200

    get serch query and get list og images

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 import apiService from './services/apiService';
+import imageTamplate from './templates/card-image.hbs';
 
-// import { refs } from "./utils/refs";
 const refs = {
   searchForm: document.querySelector('#search-form'),
   galeryList: document.querySelector('#galery-list'),
 };
+
+refs.searchForm.addEventListener('submit', searchFormSubmitHandler);
+
+function buildListImageMarkup(items) {
+  const markup = items.map((item) => imageTamplate(item)).join('');
+  refs.galeryList.insertAdjacentHTML('beforeend', markup);
+}
+
 function searchFormSubmitHandler(event) {
   event.preventDefault();
   const searchQuery = event.currentTarget.elements.query.value;
-  console.log(searchQuery);
-  apiService.fetchImages(searchQuery).then((data) => console.log(data));
+  apiService.fetchImages(searchQuery).then((data) => {
+    buildListImageMarkup(data);
+  });
 }
-refs.searchForm.addEventListener('submit', searchFormSubmitHandler);
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,11 +1,20 @@
-const URL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal';
+const URL =  'https://cors-anywhere.herokuapp.com/https://pixabay.com/api/?key=14836280-095028a335045ad546bd82bf5&image_type=photo&orientation=horizontal';
 export default {
   page: 1,
+  per_page: 6,
   fetchImages(query) {
     const options = {
       headers: { Autorization: '14836280-095028a335045ad546bd82bf5' },
     };
-    const requestParams = `?q=${query}&page=${this.page}`;
-    fetch(URL + requestParams, options).then((response) => response.json());
+    const requestParams = `&q=${query}&page=${this.page}&per_page=${this.per_page}`;
+    return fetch(URL + requestParams, options)
+      .then((response) => response.json())
+      .then((parsedResponse) => {
+        this.incrementPage();
+        return parsedResponse.hits;
+      });
+  },
+  incrementPage() {
+    this.page += 1;
   },
 };
